ad3b82de4d7fba0f87348ada871a2811bd8e524d
Praveen Kumar Sridhar <prasri.pk@gmail.com>
2022-03-14 18:02:09 -0400

    Folder stucture

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='SQuAD - Question Answering model ',
+    author='Praveen Kumar Sridhar',
+    license='MIT',
+)
